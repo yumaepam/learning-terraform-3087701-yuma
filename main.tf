ba49a577457b5951a1775a779584bdcfa0191a60
jurijs_martinens <jurijs_martinens@epam.com>
2025-03-31 16:53:05 +0300

    Create security group module

--- main.tf
+++ main.tf
@@ -44,30 +44,3 @@ resource "aws_security_group" "blog" {
   description = "Allow http and https in, Allow everything out"
   vpc_id = data.aws_vpc.default.id
 }
-
-resource "aws_security_group_rule" "blog_http_in" {
-  from_port         = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  to_port           = 80
-  type              = "ingress"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.blog.id
-  to_port           = 443
-  type              = "ingress"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.blog.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks = ["0.0.0.0/0"]
-}
